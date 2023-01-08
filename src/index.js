4f483a01ccff7400a412d35e1bad70910ac50412
Rishav Raj <91628734+Rishavraaj@users.noreply.github.com>
2023-01-08 23:11:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https') ? true : false;
+  }
+  
+  isSecure() {
+    return this.#secure;
+  }
+  
+  updateUrl(url) {
+    this.url = url;
+    this.#secure = url.startsWith('https') ? true : false;
+  }
+}
+
+const s = new API('http://api.com/api/hello');
+console.log(s.isSecure()); // false
+
+s.updateUrl('https://api.com/api/hello');
+console.log(s.isSecure()); // true
+
+console.log(s.url); // https://api.com/api/hello
+
+console.log(s.method); // GET
+
+console.log(s.secure); // undefined (because private field)
+
 
 module.exports = { API }

7e1b1d4840e2e856ee5663c06f51b374948662e5
Rishav Raj <91628734+Rishavraaj@users.noreply.github.com>
2023-02-11 15:36:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
 
-class API {
-  constructor(url, method = 'GET') {
-    this.url = url;
-    this.method = method;
-    this.#secure = url.startsWith('https') ? true : false;
-  }
-  
-  isSecure() {
-    return this.#secure;
-  }
-  
-  updateUrl(url) {
-    this.url = url;
-    this.#secure = url.startsWith('https') ? true : false;
-  }
+
+class API{
+  constructor(url)
+  this.url = url
+  this.method = "GET"
+  this.#secure = url.startWith("https");
 }
-
-const s = new API('http://api. com/api/hello');
-console.log(s.isSecure()); // false
-
-s.updateUrl('https://api. com/api/hello');
-console.log(s.isSecure()); // true
-
-console.log(s.url); // https://api.com/api/hello
-
-console.log(s.method); // GET
-
-console.log(s.secure); // undefined (because private field)
-
-
+isSecure() {
+    return this.#secure
+}
+updateURL(url) {
+  this.url = url
+}
+let firstAPI = new API('http://api.com/api/hello')
+console. log(s.isSecure()) // false
+let secondAPI = new API('https://api.com/api/hello')
+console. log(s.isSecure()) // true
+    
 module.exports = { API }
